3cacf9de9379d72a4f01c0b3aea6dc7fa69fe155
zrutao <1585240709@qq.com>
2020-01-13 00:11:50 +0800

    添加了方法test1

--- src/main/java/com/zrt/hi/HiApplication.java
+++ src/main/java/com/zrt/hi/HiApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HiApplication {
 
     public static void main(String[] args) {
-        System.out.println("代码运行");
         SpringApplication.run(HiApplication.class, args);
     }
 
