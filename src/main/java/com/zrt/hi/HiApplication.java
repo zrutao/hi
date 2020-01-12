05ef09dcfd6248c08626995e0f2608dc3d576361
zrutao <1585240709@qq.com>
2020-01-12 18:38:27 +0800

    修改了hi

--- src/main/java/com/zrt/hi/HiApplication.java
+++ src/main/java/com/zrt/hi/HiApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class HiApplication {
 
     public static void main(String[] args) {
+        System.out.println("代码运行");
         SpringApplication.run(HiApplication.class, args);
     }
 
