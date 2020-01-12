718272e71c2fc42c5652486204dc9c611f352c98
zrutao <1585240709@qq.com>
2020-01-12 15:39:16 +0800

    hi项目

---
+++ src/main/java/com/zrt/hi/HiApplication.java
@@ -0,0 +1,13 @@
+package com.zrt.hi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiApplication.class, args);
+    }
+
+}
