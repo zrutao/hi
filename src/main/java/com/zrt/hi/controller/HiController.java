4171c501b08249c21679cf423e31f1005b5f8e32
zrutao <1585240709@qq.com>
2020-01-13 17:51:44 +0800

    重新提交下

--- src/main/java/com/zrt/hi/controller/HiController.java
+++ src/main/java/com/zrt/hi/controller/HiController.java
@@ -1,11 +1,16 @@
 package com.zrt.hi.controller;
 
+import com.zrt.hi.entity.Stu;
+import com.zrt.hi.repository.HiRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 
 @RestController
@@ -13,6 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class HiController {
 
     private final static Logger logger= LoggerFactory.getLogger(HiController.class);
+
+    @Autowired
+    private HiRepository hiRepository;
+
     /**
      * 设置年龄
      */
@@ -21,5 +30,26 @@ public class HiController {
     {
         logger.info("嗨！听说你喜欢火");
         return "name:"+my;
+    }
+    /**
+     * 插入一个学生信息
+     */
+    @GetMapping(value = "/create")
+    public String create()
+    {
+        Stu stu=new Stu();
+        stu.setName("zhaorutao");
+        stu.setAge(23);
+        hiRepository.save(stu);
+        return "成功了";
+    }
+
+    /**
+     * 查询全部
+     */
+    @GetMapping("/select")
+    public List<Stu> select()
+    {
+        return hiRepository.findAll();
     }
 }
