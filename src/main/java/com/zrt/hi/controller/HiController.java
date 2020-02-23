14341d4e4203c7414775f93e4d3f3a0f78fb57ed
zrutao <1585240709@qq.com>
2020-02-23 14:53:14 +0800

    重新提交下

--- src/main/java/com/zrt/hi/controller/HiController.java
+++ src/main/java/com/zrt/hi/controller/HiController.java
@@ -1,15 +1,18 @@
 package com.zrt.hi.controller;
 
+import com.zrt.hi.entity.Result;
 import com.zrt.hi.entity.Stu;
 import com.zrt.hi.repository.HiRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 
 
@@ -28,27 +31,31 @@ public class HiController {
     @GetMapping(value = "/name")
     public String name(@RequestParam(value = "name",required = false,defaultValue = "0") String my)
     {
-        logger.info("嗨！听说你喜欢火");
+        logger.info("嗨！听说你喜欢火！！！！！");
         return "name:"+my;
     }
     /**
      * 插入一个学生信息
      */
     @GetMapping(value = "/create")
-    public Stu create(Stu s)
+    public Result<Stu> create(@Valid Stu s, BindingResult bindingResult)
     {
+
+        Result result=new Result();
         Stu stu=new Stu();
+        if(bindingResult.hasErrors())
+            {
+                result.setCode(0);
+                result.setMsg("失败");
+                return result;
+            }
         stu.setName(s.getName());
         stu.setAge(s.getAge());
         hiRepository.save(stu);
-
-        Stu stu2=new Stu();
-        stu2.setName("赵汝涛");
-        stu2.setAge(23);
-        hiRepository.save(stu2);
-
-
-        return stu2;
+        result.setCode(1);
+        result.setMsg("成功");
+        result.setData(stu);
+        return result;
     }
 
     /**
